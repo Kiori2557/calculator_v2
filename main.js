4160b77b51015320bdf6e4f8d46f10834e9be933
Aung Khant Paing <akpaing99@gmail.com>
2023-11-13 14:53:05 +0630

    Fix the input display & Added AC button function
    -Add hidden display to calculate and the input display will only display
    the input.So the user can see the previous inputs instead of previous
    result when calculate multiple times

--- main.js
+++ main.js
@@ -1,61 +1,95 @@
 let operand1 = "";
 let operand2 = "";
 let operator = "";
+let result = "";
 let isLastInputOperator = false;
 
 const numberBtns = document.querySelectorAll(".numberBtn");
 const operatorBtns = document.querySelectorAll(".operatorBtn");
 const btns = document.querySelectorAll("button");
+const allClear = document.querySelector(".clearBtn");
 const inputDisplay = document.querySelector(".input");
+const resultDisplay = document.querySelector(".result");
+const hiddenDisplay = document.querySelector(".calculation");
+
 numberBtns.forEach((numberBtn) => {
   numberBtn.addEventListener("click", () => {
-    isLastInputOperator = false;
     appendNumber(numberBtn);
-  });
-});
-operatorBtns.forEach((operatorBtn) => {
-  operatorBtn.addEventListener("click", () => {
-    if (inputDisplay.textContent) {
-      isLastInputOperator = true;
-      if (!operand1) {
-        operand1 = inputDisplay.textContent;
-      }
-      appendNumber(operatorBtn);
-      operator = operatorBtn.textContent;
+    isLastInputOperator = false;
+    if (operand1) {
+      operand2 += numberBtn.textContent;
+      calculate(operand1, operand2, operator);
+      console.log(operand1);
+      console.log(operator);
+      console.log(operand2);
     }
   });
 });
 
-function appendNumber(btn) {
-  inputDisplay.textContent += btn.textContent;
-  assignVariable(btn);
-  //   console.log(operand1);
-  //   console.log(operand2);
-  //   console.log(operator);
+operatorBtns.forEach((operatorBtn) => {
+  operatorBtn.addEventListener("click", () => {
+    if (inputDisplay.textContent) {
+      if (result) {
+        operand1 = "";
+        operand2 = "";
+        hiddenDisplay.textContent = result;
+      }
+      if (!operand1) {
+        operand1 = hiddenDisplay.textContent;
+      }
+      if (isLastInputOperator) {
+        inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
+        hiddenDisplay.textContent = hiddenDisplay.textContent.slice(0, -1);
+      }
+      operator = operatorBtn.textContent;
+      appendNumber(operatorBtn);
+      isLastInputOperator = true;
+    }
+  });
+});
+
+allClear.addEventListener("click", resetAll);
+
+function resetAll() {
+  operand1 = "";
+  operand2 = "";
+  operator = "";
+  operator = "";
+  result = "";
+  isLastInputOperator = false;
+  inputDisplay.textContent = "";
+  hiddenDisplay.textContent = "";
+  resultDisplay.textContent = "";
 }
 
-function assignVariable(btn) {
-  if (operand1 && operator) {
-    operand2 += btn.textContent;
-    calculate(operand1, operand2, operator);
-  }
+function appendNumber(btn) {
+  inputDisplay.textContent += btn.textContent;
+  hiddenDisplay.textContent += btn.textContent;
 }
 
 function calculate(operand1, operand2, operator) {
+  operand1 = Number(operand1);
+  operand2 = Number(operand2);
   switch (operator) {
     case "−":
-      console.log(subtract(operand1, operand2));
+      result = subtract(operand1, operand2);
       break;
     case "+":
-      console.log(add(operand1, operand2));
+      result = add(operand1, operand2);
       break;
     case "÷":
-      console.log(multiply(operand1, operand2));
+      result = divide(operand1, operand2);
       break;
     case "×":
-      console.log(divide(operand1, operand2));
+      result = multiply(operand1, operand2);
       break;
   }
+  result = roundNumber(result);
+  resultDisplay.textContent = result;
+}
+
+function roundNumber(number) {
+  return Math.round(number * 1000) / 1000;
 }
 
 function add(operand1, operand2) {
